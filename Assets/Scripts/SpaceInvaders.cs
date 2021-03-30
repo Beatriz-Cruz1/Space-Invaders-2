2e3c84588dc6f6b1c11a6ea8c44edd19001bec81
Beatriz-Cruz1 <80706849+Beatriz-Cruz1@users.noreply.github.com>
2021-03-30 20:15:46 +0100

    INVADERS

---
+++ Assets/Scripts/SpaceInvaders.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceInvaders : MonoBehaviour
+{
+    [SerializeField]
+    GameObject invaderA;
+
+    [SerializeField]
+    GameObject invaderB;
+
+    [SerializeField]
+    int nInvaders = 7;
+
+    [SerializeField]
+    float xMin = -3f;
+    void Awake()
+    {
+        float x = xMin;
+        for(int i = 1; i<= nInvaders; i++)
+        {
+            GameObject newinvader = Instantiate(invaderA, transform);
+            newinvader.transform.position = new Vector3( x, -0.5f, 0f);
+            x += 1f; 
+        }
+        
+    }
+
+}
