4e88e8cf682ec6c9796d3ec31db3dc1963f8e036
Beatriz-Cruz1 <80706849+Beatriz-Cruz1@users.noreply.github.com>
2021-04-01 11:41:25 +0100

    Change 2

---
+++ Assets/Scripts/Invader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invader : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Fire;
+
+    [SerializeField]
+    float cadencia = 1.5f;
+
+    float tempoQuePassou = 0f;
+
+    private void Update()
+    {
+        tempoQuePassou += Time.deltaTime;
+        if(tempoQuePassou >= cadencia)
+        {
+           Instantiate(fire)
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+        Destroy(collision.gameObject);
+    }
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    [SerializeField]
+    GameObject fire;
+
+    [SerializeField]
+    GameObject nozzle;
+
+    [SerializeField]
+    float velocidade = 5f;
+
+    Vector3 minX, maxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minX = Camera.main.ViewportToWorldPoint(Vector2.zero).x + 0.5;
+        maxX = Camera.main.ViewportToWorldPoint(Vector2.one).x - 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(fire, nozzle.position, nozzle.rotation);
+        }
+
+        MoveShip();
+
+        void MoveShip()
+        {
+            float hMov = GetAxis("Horizontal");
+            transform.position += hMov * velocidade * Vector3.right * Time.deltaTime;
+
+            Vector3 position = transform.position;
+            position.x = Mathf.Clamp(position.x, minX.x, maxX.x);
+            transform.position = position;
+        }
+        
+    }
+}
